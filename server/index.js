ba132fe08c97f02b1831f7985c9e11a827e25731
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-12-15 16:59:30 +0200

    added perMessageDeflate

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  perMessageDeflate: false //required for Web App for Containers
+});
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 let userId = 0;
-io.on('connection', function(socket){
-  socket.userId = userId ++;
+io.on('connection', function (socket) {
+  socket.userId = userId++;
   console.log('a user connected, user id: ' + socket.userId);
 
-  socket.on('chat', function(msg){
+  socket.on('chat', function (msg) {
     console.log('message from user#' + socket.userId + ": " + msg);
     io.emit('chat', {
       id: socket.userId,
@@ -20,6 +22,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
