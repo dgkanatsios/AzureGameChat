d5348709af99a35cbacd09fb32d7fefa85a30abf
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-12-15 17:24:56 +0200

    fixes

--- various/build.sh
+++ various/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t dgkanatsios/azuregamechat:0.1 .
-docker push dgkanatsios/azuregamechat:0.1
-docker run -p 3000:3000 --name azuregamechat dgkanatsios/azuregamechat:0.1
+docker build -t dgkanatsios/azuregamechatserver:0.1 .
+docker push dgkanatsios/azuregamechatserver:0.1
+docker run -p 3000:3000 --name azuregamechatserver dgkanatsios/azuregamechatserver:0.1
