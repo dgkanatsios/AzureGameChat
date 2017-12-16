764447727679b087c3a10acfd05e1ecb54299ea0
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-12-16 22:20:48 +0200

    1st Unity demo works!

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const shortid = require('shortid');
 const io = require('socket.io')(http, {
-  perMessageDeflate: false //required for Web App for Containers
+  perMessageDeflate: false //required for Web App for Containers on Azure
 });
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-let userId = 0;
 io.on('connection', function (socket) {
-  socket.userId = userId++;
-  console.log('a user connected, user id: ' + socket.userId);
+  socket.userId = shortid.generate();
+  console.log(`a user connected, user id: ${socket.userId}`);
 
   socket.on('chat', function (msg) {
-    console.log('message from user#' + socket.userId + ": " + msg);
+    console.log(`message from user# ${socket.userId}: ${msg}`);
     io.emit('chat', {
       id: socket.userId,
       msg: msg
     });
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
 
