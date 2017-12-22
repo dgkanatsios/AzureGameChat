99c55d1fcbf0c34f3ecab2d3c6530c3645e97f12
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-12-22 19:00:15 +0200

    Dockerfile runs as non-root user

--- various/build.sh
+++ various/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t dgkanatsios/azuregamechatserver:0.1 .
-docker push dgkanatsios/azuregamechatserver:0.1
-docker run -p 3000:3000 --name azuregamechatserver dgkanatsios/azuregamechatserver:0.1
+docker build -t dgkanatsios/azuregamechatserver:0.4 .
+docker push dgkanatsios/azuregamechatserver:0.4
+docker run -p 3000:3000 --name azuregamechatserver dgkanatsios/azuregamechatserver:0.4
